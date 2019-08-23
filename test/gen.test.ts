eba5256e4b4871429447d27313d4705e8f273b02
kingwl <kingwenlu@gmail.com>
2019-08-23 16:05:03 +0800

    add tests

--- test/gen.test.ts
+++ test/gen.test.ts
@@ -1,7 +1,7 @@
 import VirtualMachine from "../src/vm";
 import { gen } from "../src/gen";
 
-test(`default`, () => {
+test(`should work with condition`, () => {
   const code = '0 ? 2 : 0 ? 3 : 4'
   const [op, value] = gen(code)
   const vm = new VirtualMachine(op, value)
@@ -21,6 +21,17 @@ test(`should work with def and load`, () => {
   const code = `
     var a = 1;
     a;
+  `
+  const [op, value] = gen(code)
+  const vm = new VirtualMachine(op, value)
+  
+  expect(vm.exec().value).toBe(eval(code))
+})
+
+test(`should work with var and cond`, () => {
+  const code = `
+    var a = 1;
+    a ? a + 1 : 0;
   `
   const [op, value] = gen(code)
   const vm = new VirtualMachine(op, value)
