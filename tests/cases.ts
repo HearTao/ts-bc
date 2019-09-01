608791099d9025ca7220998c3b87845ac0e3905f
kingwl <kingwenlu@gmail.com>
2019-09-01 15:16:34 +0800

    wip

--- tests/cases.ts
+++ tests/cases.ts
@@ -1,25 +1,31 @@
 import VirtualMachine, { gen, JSString, VObject } from '../src'
 
 const code = `
-function foo() {
-  var a = 0;
-  function bar () {
-    a += 1;
-    for (let i = 0; i < 10; ++i) {
-      a = a + i - i
-    }
-    return a;
+
+function numJewelsInStones(J, S) {
+  let set = {}
+  for (let v of J) {
+      set[v] = true
   }
-  return bar;
-}
-var b = foo();
-b();
-b()
+  let c = 0;
+  for(let i = 0; i < S.length; ++i) {
+      if (set[S[i]]) {
+          c += 1
+      }
+  }
+  return c
+};
+
+numJewelsInStones(a, b)
 `
 const [op, value] = gen(code)
 
-const vm = new VirtualMachine(op, value)
+console.log(op, value)
+const vm = new VirtualMachine(op, value, 0, valueTable => {
+  valueTable.set('a', new JSString('aA'))
+  valueTable.set('b', new JSString('aAAbbbb'))
+})
 
 console.log(`code: ${code}`)
 console.log(`vm: ${vm.exec().value.debugValue()}`)
-console.log(`eval: ${eval(code)}`)
+// console.log(`eval: ${eval(code)}`)
